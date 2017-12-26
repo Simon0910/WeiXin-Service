424849b6c192383b39545a35875d951c274cfdd1
yangzhan <yangzhan@minghang.im>
2017-12-26 17:41:52 +0800

    Create Program

---
+++ WeiXin-Subscribe/src/main/java/com/young/minico/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.young.minico.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	Logger logger = LoggerFactory.getLogger(TestController.class);
+	@RequestMapping("test")
+	public String test()
+	{
+		logger.info("testddddddddddddddddddddd");
+		return "ok";
+	}
+}
