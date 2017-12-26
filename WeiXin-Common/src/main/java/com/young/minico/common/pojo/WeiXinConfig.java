4b400fc387682b04600a907464544c4a3fb352e1
yangzhan <yangzhan@minghang.im>
2017-12-26 18:23:36 +0800

    create

---
+++ WeiXin-Common/src/main/java/com/young/minico/common/pojo/WeiXinConfig.java
@@ -0,0 +1,26 @@
+package com.young.minico.common.pojo;
+
+//@ConfigurationProperties(prefix = "WeiXinConfig")
+public class WeiXinConfig {
+	private String grantType = "client_credential";
+	private String appID ;
+	private String secret ;
+	public String getGrantType() {
+		return grantType;
+	}
+	public void setGrantType(String grantType) {
+		this.grantType = grantType;
+	}
+	public String getAppID() {
+		return appID;
+	}
+	public void setAppID(String appID) {
+		this.appID = appID;
+	}
+	public String getSecret() {
+		return secret;
+	}
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+}
---
+++ WeiXin-Common/src/main/java/com/young/minico/common/service/AccessTokenService.java
@@ -0,0 +1,9 @@
+package com.young.minico.common.service;
+
+//@Service
+public class AccessTokenService {
+	public void getAccessToken()
+	{
+//		https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+	}
+}
